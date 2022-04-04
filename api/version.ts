225855c437875d33d45d47fdb4c6df1478e9ae9f
positionexchanger <positionexchange.holder@gmail.com>
2022-04-04 11:56:58 +0700

    chore: add endpoint

--- api/version.ts
+++ api/version.ts
@@ -1,6 +1,7 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
 import { version } from '../package.json'
+import { endpoint } from '../utils/endpoint'
 
 export default (req: VercelRequest, res: VercelResponse): void => {
-  res.json({ version })
+  res.json({ version, endpoint })
 }
---
+++ utils/endpoint.ts
@@ -0,0 +1,6 @@
+export const endpoint = {
+  v1: {
+    prices: '/api/v1/prices',
+    address: '/api/v1/address/{{address}}'
+  }
+}
